d1f95c28585759acf48c41ef131eba88d97babd2
Yab1 <yabilisanu@gmail.com>
2023-07-18 03:01:32 +0300

    Config: Create tailwind.config.js file to customize and configure Tailwind CSS settings for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,8 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          800: "#27272a",
-          900: "#18181b",
-        },
-        green: "#22c55e",
-        black: "#020617",
-        white: "#f8fafc",
-      },
       fontFamily: {
-        body: ["Nunito", "Roboto"],
+        body: ["Nunito"],
       },
     },
   },
