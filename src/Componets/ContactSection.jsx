f8235847864c0f8d56b87bc65b3e1751d617fc39
Yab1 <yabilisanu@gmail.com>
2023-07-19 14:06:33 +0300

    Style: Apply CSS styling to enhance the appearance of the section title for improved visual presentation

--- src/Componets/ContactSection.jsx
+++ src/Componets/ContactSection.jsx
@@ -5,6 +5,18 @@ export default class ContactSection extends Component {
     super();
   }
   render() {
-    return <div>ContactSection</div>;
+    return (
+      <section id="contact" className="h-screen py-5">
+        <title className="flex flex-col justify-center select-none border-2 w-screen">
+          <span className="self-center font-body text-lg md:text-2xl lg:text-4xl font-black text-white relative uppercase z-10 ">
+            get in
+            <span className="text-yellow-400 uppercase"> touch</span>
+          </span>
+          <span className="text-zinc-600 font-body text-3xl md:text-5xl lg:text-7xl font-black absolute self-center opacity-30 tracking-wider uppercase z-0 ">
+            contact
+          </span>
+        </title>
+      </section>
+    );
   }
 }
--- src/Componets/Footer.jsx
+++ src/Componets/Footer.jsx
@@ -5,6 +5,18 @@ export default class Footer extends Component {
     super();
   }
   render() {
-    return <div>Footer</div>;
+    return (
+      <section id="blog" className="h-screen py-5">
+        <title className="flex flex-col justify-center select-none border-2 w-screen">
+          <span className="self-center font-body text-lg md:text-2xl lg:text-4xl font-black text-white relative uppercase z-10 ">
+            my
+            <span className="text-yellow-400 uppercase"> blog</span>
+          </span>
+          <span className="text-zinc-600 font-body text-3xl md:text-5xl lg:text-7xl font-black absolute self-center opacity-30 tracking-wider uppercase z-0 ">
+            posts
+          </span>
+        </title>
+      </section>
+    );
   }
 }
