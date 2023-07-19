a28c4e0a956e429fc6b633e37278f301b0ba146a
Yab1 <yabilisanu@gmail.com>
2023-07-19 14:09:12 +0300

    Style: Apply CSS styling to enhance the appearance of the section title for improved visual presentation

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -8,7 +8,7 @@ export default class Header extends Component {
 
   render() {
     return (
-      <header className="font-body text-white bg-zinc-800 flex justify-between pt-8 px-10 xl:px-40">
+      <header className="font-body text-white bg-zinc-800 flex justify-between py-5 px-5 md:px-10 lg:px-40">
         <h3 className="font-semibold cursor-default text-2xl">
           &lt;/<span className="text-yellow-400">Yeabsera</span>&gt;
         </h3>
