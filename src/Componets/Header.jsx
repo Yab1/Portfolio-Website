e5ea874fb17ef4eb1d6a1c55c75ea36ad2784cde
Yab1 <yabilisanu@gmail.com>
2023-07-24 15:19:45 +0300

    Feature: Implement animation using Framer Motion to create an animated header for an engaging and dynamic user experience.

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { Lightbulb, LightbulbFill } from "react-bootstrap-icons";
+import { motion } from "framer-motion";
 export default class Header extends Component {
   constructor(props) {
     super(props);
@@ -7,11 +8,25 @@ export default class Header extends Component {
   }
 
   render() {
+    const headerVariants = {
+      hidden: {
+        y: -250,
+      },
+      visible: {
+        y: 0,
+      },
+    };
+
     return (
       <header className="font-body text-white bg-zinc-800 flex justify-between py-5 px-5 md:px-10 lg:20 xl:px-40">
-        <h3 className="font-semibold cursor-default text-2xl">
+        <motion.h3
+          className="font-semibold cursor-default text-2xl"
+          variants={headerVariants}
+          initial="hidden"
+          animate="visible"
+        >
           &lt;/<span className="text-yellow-400">Yeabsera</span>&gt;
-        </h3>
+        </motion.h3>
       </header>
     );
   }
