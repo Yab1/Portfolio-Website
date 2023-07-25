68d7bf3e68ff3cf2144b55da3bcccde0fb9f9a8a
Yab1 <yabilisanu@gmail.com>
2023-07-25 14:50:48 +0300

    Feature: Add Framer Motion animation to the navigation section for smooth and visually appealing navigation transitions.

--- src/Componets/Navigation.jsx
+++ src/Componets/Navigation.jsx
@@ -10,36 +10,54 @@ import {
   Envelope,
 } from "react-bootstrap-icons";
 
+// Framer Motion
+import { motion } from "framer-motion";
+
 export default class Navigation extends Component {
   render() {
+    const buttonVariants = {
+      hover: {
+        scale: 1.1,
+        backgroundColor: "#facc15",
+        color: "black",
+      },
+    };
     return (
       <div className="flex justify-center flex-col align-center w-fit gap-5 md:fixed right-5 inset-y-0">
-        <a
+        <motion.a
           href="#home"
-          className="bg-zinc-700 p-2 text-white rounded-full hover:bg-yellow-400 "
+          className="bg-zinc-700 p-2 text-white rounded-full"
+          variants={buttonVariants}
+          whileHover="hover"
         >
           <HouseDoorFill size={15} />
-        </a>
+        </motion.a>
 
-        <a
+        <motion.a
           href="#about"
-          className="bg-zinc-700 p-2 text-white rounded-full hover:bg-yellow-400 "
+          className="bg-zinc-700 p-2 text-white rounded-full"
+          variants={buttonVariants}
+          whileHover="hover"
         >
           <PersonFill size={15} />
-        </a>
-        <a
+        </motion.a>
+        <motion.a
           href="#works"
-          className="bg-zinc-700 p-2 text-white rounded-full hover:bg-yellow-400 "
+          className="bg-zinc-700 p-2 text-white rounded-full"
+          variants={buttonVariants}
+          whileHover="hover"
         >
           <Briefcase size={15} />
-        </a>
+        </motion.a>
 
-        <a
+        <motion.a
           href="#contact"
-          className="bg-zinc-700 p-2 text-white rounded-full hover:bg-yellow-400 "
+          className="bg-zinc-700 p-2 text-white rounded-full"
+          variants={buttonVariants}
+          whileHover="hover"
         >
           <Envelope size={15} />
-        </a>
+        </motion.a>
       </div>
     );
   }
