8cdbae5b7e31b2055650873b090ab431c1e93427
Yab1 <yabilisanu@gmail.com>
2023-07-25 14:50:13 +0300

    Feature: Apply animation using Framer Motion to the contact section for an interactive and visually appealing contact form.

--- src/Componets/ContactSection.jsx
+++ src/Componets/ContactSection.jsx
@@ -8,18 +8,43 @@ import {
   Telegram,
   Send,
 } from "react-bootstrap-icons";
+import { motion } from "framer-motion";
 
 export default class ContactSection extends Component {
   constructor() {
     super();
   }
   render() {
+    const titleVariants = {
+      hidden: {
+        opacity: 0,
+      },
+      visible: {
+        opacity: 1,
+        transition: {
+          duration: 1.5,
+          delay: 0.2,
+        },
+      },
+    };
+    const buttonVariants = {
+      hover: {
+        scale: 1.1,
+        backgroundColor: "#facc15",
+        color: "black",
+      },
+    };
     return (
       <section
         id="contact"
         className="h-screen font-body py-5 px-5 md:px-10 lg:20 xl:px-40"
       >
-        <title className="flex flex-col justify-center select-none">
+        <motion.title
+          className="flex flex-col justify-center select-none"
+          variants={titleVariants}
+          initial="hidden"
+          whileInView="visible"
+        >
           <span className="self-center text-2xl md:text-3xl lg:text-4xl font-black text-white relative uppercase z-10 ">
             get in
             <span className="text-yellow-400 uppercase"> touch</span>
@@ -27,13 +52,18 @@ export default class ContactSection extends Component {
           <span className="text-zinc-600 text-5xl md:text-5xl lg:text-7xl font-black absolute self-center opacity-30 tracking-wider uppercase z-0 ">
             contact
           </span>
-        </title>
+        </motion.title>
         <section className="flex flex-col-reverse lg:flex-row gap-16 my-10">
           {/* Left Section */}
-          <section className="flex flex-col gap-5 lg:w-8/12">
-            <p className="uppercase text-white text-md lg:text-lg font-extrabold">
+          <motion.section
+            className="flex flex-col gap-5 lg:w-8/12"
+            variants={titleVariants}
+            initial="hidden"
+            whileInView="visible"
+          >
+            <motion.p className="uppercase text-white text-md lg:text-lg font-extrabold">
               Don't be shy !
-            </p>
+            </motion.p>
             <p className="text-zinc-200 text-sm lg:text-md ">
               Feel free to get in touch with me. I am always open to discussing
               new projects, creating ideas, or exploring opportunities to be a
@@ -63,17 +93,29 @@ export default class ContactSection extends Component {
               </div>
             </div>
             <div className="flex gap-5 justify-evenly">
-              <div className="p-3 text-white bg-zinc-700 w-fit rounded-full hover:bg-yellow-400 active:bg-yellow-400">
+              <motion.div
+                className="p-3 text-white bg-zinc-700 w-fit rounded-full active:bg-yellow-400 transition ease-out duration-500"
+                variants={buttonVariants}
+                whileHover="hover"
+              >
                 <Linkedin size={20} />
-              </div>
-              <div className="p-3 text-white bg-zinc-700 w-fit rounded-full hover:bg-yellow-400 active:bg-yellow-400">
+              </motion.div>
+              <motion.div
+                className="p-3 text-white bg-zinc-700 w-fit rounded-full active:bg-yellow-400 transition ease-out duration-500"
+                variants={buttonVariants}
+                whileHover="hover"
+              >
                 <Telegram size={20} />
-              </div>
-              <div className="p-3 text-white bg-zinc-700 w-fit rounded-full hover:bg-yellow-400 active:bg-yellow-400">
+              </motion.div>
+              <motion.div
+                className="p-3 text-white bg-zinc-700 w-fit rounded-full active:bg-yellow-400 transition ease-out duration-500"
+                variants={buttonVariants}
+                whileHover="hover"
+              >
                 <Github size={20} />
-              </div>
+              </motion.div>
             </div>
-          </section>
+          </motion.section>
           {/* Right Section */}
           <form action="" className="flex flex-col gap-5 lg:gap-8 lg:w-11/12">
             <div className="flex flex-col lg:flex-row gap-5">
@@ -101,12 +143,16 @@ export default class ContactSection extends Component {
               placeholder="YOUR MESSAGE"
               className="bg-zinc-700 rounded px-5 py-2 placeholder-zinc-400 focus:outline-2 focus:outline-yellow-400 text-white"
             ></textarea>
-            <div className="uppercase w-fit border-2 rounded-full pl-5 hover:bg-yellow-400 hover:text-black cursor-pointer text-white self-center">
+            <motion.div
+              className="uppercase w-fit border-2 rounded-full pl-5 cursor-pointer text-white self-center transition ease-out duration-500"
+              variants={buttonVariants}
+              whileHover="hover"
+            >
               Send message
               <button className="rounded-full bg-yellow-400 p-3 ml-3">
                 <Send />
               </button>
-            </div>
+            </motion.div>
           </form>
         </section>
       </section>
