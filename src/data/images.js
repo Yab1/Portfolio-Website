813d7ead27457ee0c75d1054d89d6bb32ab8f363
Yab1 <yabilisanu@gmail.com>
2023-08-10 21:18:49 +0300

    Data: Added image object.

---
+++ src/data/images.js
@@ -0,0 +1,7 @@
+import quizzical from "../assets/projects/quizzical.png";
+import simpleNotes from "../assets/projects/simpleNotes.png";
+
+export const IMAGES = [
+  { id: 0, name: quizzical },
+  { id: 1, name: simpleNotes },
+];
