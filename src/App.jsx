17eec8e141f4e4616e514721fb8a5fcbc9d13e7c
Yab1 <yabilisanu@gmail.com>
2023-08-10 21:12:59 +0300

    Script: Hide button on hero section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,78 @@
+// React imports
+import { Component, createRef } from "react";
+
 // Components
-import Header from "./Componets/Header";
-import Navigation from "./Componets/Navigation";
-import HeroSection from "./Componets/HeroSection";
-import AboutSection from "./Componets/AboutSection";
-import WorkSection from "./Componets/WorkSection";
-import ContactSection from "./Componets/ContactSection";
-import Footer from "./Componets/Footer";
+import Header from "./Components/Header";
+import Navigation from "./Components/Navigation";
+import HeroSection from "./Components/HeroSection";
+import AboutSection from "./Components/AboutSection";
+import WorkSection from "./Components/WorkSection";
+import ContactSection from "./Components/ContactSection";
+import Footer from "./Components/Footer";
 
-function App() {
-  return (
-    <div className="">
-      <Header />
-      <Navigation />
-      <HeroSection />
-      <AboutSection />
-      <WorkSection />
-      <ContactSection />
-      <Footer />
-    </div>
-  );
+// Bootstrap icons imports
+import { ArrowUpCircle } from "react-bootstrap-icons";
+
+// Framer Motion import
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    backgroundColor: "#facc15",
+    color: "#020617",
+  },
+};
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showAnchor: false,
+    };
+    this.anchorRef = createRef();
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    const about = document.querySelector("#about");
+    let scrollY = window.scrollY;
+    if (scrollY < about.offsetTop - 500) {
+      this.setState({ showAnchor: false });
+    } else {
+      this.setState({ showAnchor: true });
+    }
+  }
+  render() {
+    return (
+      <div className="">
+        <Header />
+        <Navigation />
+        <HeroSection />
+        <AboutSection />
+        <WorkSection />
+        <ContactSection />
+        <Footer />
+        {this.state.showAnchor && (
+          <motion.a
+            ref={this.anchorRef}
+            id="go-to-home"
+            href="#home"
+            className="bg-zinc-700 p-2 text-white rounded-full fixed bottom-5 right-5"
+            variants={buttonVariants}
+            whileHover="hover"
+          >
+            <ArrowUpCircle size={15} />
+          </motion.a>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
