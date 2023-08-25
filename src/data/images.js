3b9cb0241a407362317359c988156026f5ccc4e3
Yab1 <yabilisanu@gmail.com>
2023-08-26 00:24:07 +0300

    Add new imports

--- src/data/images.js
+++ src/data/images.js
@@ -1,7 +1,17 @@
 import quizzical from "../assets/projects/quizzical.png";
 import simpleNotes from "../assets/projects/simpleNotes.png";
+import wayFinderPlus from "../assets/projects/way-finder-plus.png";
+import wayFinderPlusAdmin from "../assets/projects/way-finder-plus-admin.png";
+import wayFinderPlusVR from "../assets/projects/way-finder-plus-vr.png";
+import portfolioTwo from "../assets/projects/portfolio-two.png";
+import portfolioThree from "../assets/projects/portfolio-three.png";
 
 export const IMAGES = [
-  { id: 0, name: quizzical },
-  { id: 1, name: simpleNotes },
+  { id: 0, name: wayFinderPlus },
+  { id: 1, name: wayFinderPlusVR },
+  { id: 2, name: portfolioTwo },
+  { id: 3, name: simpleNotes },
+  { id: 4, name: portfolioThree },
+  { id: 5, name: wayFinderPlusAdmin },
+  { id: 6, name: quizzical },
 ];
